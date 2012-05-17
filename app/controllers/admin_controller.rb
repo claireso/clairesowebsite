fd6c98407b028453c1c871d620cc5542d5a217ed
claireso <claire.sosset@gmail.com>
2012-05-17 16:10:48 +0200

    add layout admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,9 @@
 class AdminController < ApplicationController
   require './config/protect'
-  http_basic_authenticate_with :login => LOGIN, :password => PASSWORD
+  http_basic_authenticate_with :name => NAME, :password => PASSWORD
+
+  layout "admin"
+
   def index
   end
 end
