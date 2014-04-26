b15df3a5cb7c207fef9d648537743bd275e36237
Claire Sosset <claire.sosset@gmail.com>
2014-04-26 20:53:43 +0200

    updating sizes of projects cover

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
     validates :title, presence: true
     validates :url, presence: true
-    has_attached_file :cover, :styles => { :medium => "400x240>" }, :default_url => ''
+    has_attached_file :cover, :styles => { :medium => "400x200>" }, :default_url => ''
     validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
 end
