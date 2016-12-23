627f058a7b356f5575b4d3d05905ef6c3d9a12d4
Claire Sosset <claire.sosset@gmail.com>
2016-12-23 12:13:01 +0100

    sw: manage caches

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
+const CURRENT_CACHE = 'static-v1';
+
+const expectedCaches = [
+  CURRENT_CACHE,
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('static-v1').then(cache => cache.addAll([
+    caches.open(CURRENT_CACHE).then(cache => cache.addAll([
       '/',
       'index.html',
       '/favicon.png',
@@ -8,10 +14,26 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
+self.addEventListener('activate', event => {
+  // remove old caches
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (!/^static-/.test(cacheName)) {
+            return;
+          }
+          if (expectedCaches.indexOf(cacheName) == -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(r => r || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
