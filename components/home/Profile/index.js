ea9b754c4e810687a16a5879d4ee787ff6c4d11c
claire <claire.sosset@gmail.com>
2018-11-14 15:22:13 +0100

    :sparkles: new website

--- components/home/Profile/index.js
+++ components/home/Profile/index.js
@@ -11,12 +11,12 @@ const Profile = () => {
           <Text>
             Je suis Claire Sosset, développeuse front-end depuis maintenant 9
             ans. <br />
-            Durant ces années j'ai travaillé au sein d'agences web comme de
+            Durant ces années j'ai travaillé au sein d'agences web et de
             startups (Octave & Octave, Ulule), ce qui m'a permis de prendre part
-            à l'élaboration de sites corporate comme d' applications javascript
+            à l'élaboration de sites corporate et d' applications javascript
             complexes. <br />
-            J'apprécie tout particulièrement développer de belles interfaces en
-            collaboration avec des UI/UX designers et les dynamiser à l'aide
+            J'apprécie tout particulièrement de développer de belles interfaces en
+            collaboration avec des UI/UX designers et de les dynamiser à l'aide
             d'API et d'outils modernes (React / Redux / Next / Webpack/
             StyledComponents ...).
           </Text>
